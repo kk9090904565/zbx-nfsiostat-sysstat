ecbae239b3569d2c2cfd9ff4b719ea93abf30fbd
kk9090904565 <89518195+kk9090904565@users.noreply.github.com>
2021-08-25 13:29:28 +0300

    Update mount-nfs-discovery.sh

--- mount-nfs-discovery.sh
+++ mount-nfs-discovery.sh
@@ -7,6 +7,7 @@ for nfs_mount in ${nfs_mounts[@]}
 do
     echo -n "    {\"{#MOUNT_POINT}\":\"$nfs_mount\"}," 
 done | sed 's/,$//' 
+# Added sed in the end of for loop, because old zabbix doesn't accept json with trailing commas
 
 echo "]" 
 echo "}" 
